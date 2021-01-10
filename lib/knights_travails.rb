af2b5a970bb716145a083146dbc197de83e6459e
Harold Parra <gc.harold.parra@gmail.com>
2021-01-10 04:50:52 +0000

    renamed some methods and updated rspec tests

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,2 +1,33 @@
+class Knight
+  #POSSIBLE MOVES BY THE KNIGHT
+  @@moves = [2 => [1,-1], -2 => [1,-1], 1 => [2,-2], -1 => [2,-2]]
+
+  def initialize(position)
+    @root_position = position
+  end
+end
+
+class PositionNode < Knight
+  def initialize(position)
+    @position = position
+    @x = position[0]
+    @y = position[1]
+    @next_positions = next_positions(position)
+  end
+
+  def next_positions(position, next_positions = [])
+    @@moves.each do |move_x, key|
+      key.each do |move_y|
+        new_x = @x + move_x
+        new_y = @y + move_y
+        next_positions << PositionNode.new([new_x, new_y])
+      end
+    end
+  end
+
+  end
+
 def knight_moves(start, finish)
+  knight = Knight.new(start)
+  knight.moves_to(finish)
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -2,11 +2,11 @@ require './lib/knights_travails'
 
 describe "#knight_moves" do
   it "returns an array" do
-    expect(knight_moves([2,3],[2,5]).is_a?(Array)).to eql(true)
+   expect(knight_moves([2,3],[2,5]).is_a?(Array)).to eql(true)
   end
 
   it "returns the finish position as the last array" do
-    expect(knight_moves([3,3],[4,3])[-1]).to eql([4,3])
+    (knight_moves([3,3],[4,3])[-1]).to eql([4,3])
   end
 
   it "returns the start position as the first array" do
@@ -17,11 +17,8 @@ describe "#knight_moves" do
     expect(knight_moves([3,3],[4,3])).to eql([[3,3],[4,5],[2,4],[4,3]])
   end
 
-  #it "" do
-  #end
-
   it "returns an error for positions greater than [8,8]" do
-    expect(knight_moves([9,0],[3,4])).to eql("Invalid positions")
+   expect(knight_moves([9,0],[3,4])).to eql("Invalid positions")
   end
 
   it "returns an error for positions less than [0,0]" do
@@ -29,3 +26,12 @@ describe "#knight_moves" do
   end
 end
 
+describe Knight do
+end
+
+describe PositionNode do
+  it "Returns an array" do
+    pos_node = PositionNode.new([4,4])
+    expect(pos_node.next_positions.is_a?(Array)).to eql(true)
+  end
+end
