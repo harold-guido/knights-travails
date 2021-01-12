570ebf73b3f1da8dc51425c79d30979ce09c2744
Harold Parra <gc.harold.parra@gmail.com>
2021-01-12 14:42:08 +0000

    made and passed tests for #available? method of the Board class

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,4 +1,21 @@
+#ALL MOVE VECTORS FOR KNIGHT
 $moves = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
+
+class Board
+  def initialize()
+    @positions = []
+    8.times do |x|
+      8.times do |y|
+        @positions << [x,y]
+      end
+    end
+    @available_positions = @positions
+  end
+
+  def available?(position)
+    @available_positions.include?(position) ? true : false
+  end
+end
 
 class Knight 
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,1 +1,50 @@
 require './lib/knights_travails'
+
+
+#TESTS FOR BOARD CLASS
+describe Board do
+  before(:all) do
+    @board = Board.new
+  end
+  describe "#available?" do
+    it "returns true for positions inside board" do
+      expect(@board.available?([0,0])).to eql(true)
+      expect(@board.available?([7,5])).to eql(true)
+    end
+
+    it "returns false for positions outside board" do
+      expect(@board.available?([-1,5])).to eql(false)
+      expect(@board.available?([7,-5])).to eql(false)
+      expect(@board.available?([12,5])).to eql(false)
+      expect(@board.available?([7,8])).to eql(false)
+    end
+  end
+
+  describe "#place_knight" do
+    it "removes position from available places" do
+      @board.place_knight([2,3])
+      expect(@board.available?([2,3])).to eql(false)
+    end
+
+    it "does not give an error when the position is not available" do
+      @board.place_knight([3,3])
+      expect{ @board.place_knight([3,3]) }.not_to raise_error
+    end
+  end
+
+  describe "#remove_knight" do
+  end
+
+  describe "#clear_board" do
+  end
+end
+
+#TESTS FOR KNIGHT_MOVES METHOD
+describe "#knight_moves" do
+  it "returns an array" do
+  end
+end
+
+#TESTS FOR KNIGHT CLASS
+describe Knight do
+end
