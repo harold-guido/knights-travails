dabfbf1594d21c465131dab49d8a0bb082e02ffc
Harold Parra <gc.harold.parra@gmail.com>
2021-01-15 01:16:26 +0000

    working version of program, error when function is used consecutively

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -25,14 +25,26 @@ class Board
 end
 
 class Knight < Board 
-  attr_accessor :root_position, :next_positions
+  attr_accessor :root_node, :next_positions
 
   @@MOVES = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]]
   @@BOARD = Board.new
 
   def initialize(position)
     @root_node = Node.new(position)
-    @@BOARD.clear
+  end
+
+  #FIND FINISH NODE USING BREADTH FIRST SEARCH
+  def find_node(finish, node_queue = [@root_node], next_queue = [])
+    node_queue.each do |node|
+      if node.position == finish
+        return node
+      else
+        next_queue += node.next_positions unless node.next_positions == nil
+      end
+    end
+
+    next_queue.length == 0 ? return : find_node(finish, next_queue)
   end
 end
 
@@ -43,14 +55,31 @@ class Node < Knight
     @previous_position = previous_position
     @position = position
     @next_positions = next_positions
+  end
 
-    self.list_next
+  def self.breadth_first_list_next(node_queue, next_queue = [])
+    if node_queue.is_a?(Node)
+      node_queue.list_next()
+
+      next_queue = node_queue.next_positions
+      breadth_first_list_next(next_queue)
+    else
+      node_queue.each do |node|
+        node.list_next()
+
+        next_queue += node.next_positions unless node.next_positions == nil
+      end
+
+      if next_queue.length != 0
+        breadth_first_list_next(next_queue)
+      end
+    end
   end
 
   def list_next(next_positions = [])
     @@MOVES.each do |move|
-      new_x = @position[0] + move[0]
-      new_y = @position[1] + move[1]
+      new_x = self.position[0] + move[0]
+      new_y = self.position[1] + move[1]
 
       if @@BOARD.available?([new_x,new_y]) then
         @@BOARD.place_knight([new_x,new_y])
@@ -59,29 +88,34 @@ class Node < Knight
     end
 
     if next_positions.length != 0 then
-      @next_positions = next_positions.map{ |position| Node.new(position, self) }
+      self.next_positions = next_positions.map{ |position| Node.new(position, self) }
     else
-      @next_positions = nil
+      self.next_positions = nil
     end
   end
 
-  def list_previous_positions(previous_positions = [])
+  def list_previous(previous_positions = [])
     if @previous_position == nil then
       [@position]
     else
       previous_position = [@position] 
-      previous_position += @previous_position.list_previous_positions
+      previous_position = @previous_position.list_previous + previous_position
     end
   end
 
-  def next_positions_array()
-    return_array = []
-    @next_positions.each{ |node| return_array.push(node.position) } unless @next_positions == nil
+  def self.next_positions_array(node_array, return_array = [])
+    if node_array == nil
+      return_array = nil
+    else
+      node_array.each{ |node| return_array.push(node.position) } unless @next_positions == nil
+    end
     return_array
   end
 end
 
 def knight_moves(start,finish)
   knight = Knight.new(start)
-  knight.bfs(finish).list_previous_positions
+  Node.breadth_first_list_next(knight.root_node)
+
+  return knight.find_node(finish).list_previous
 end
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -52,25 +52,61 @@ end
 
 #TESTS FOR KNIGHT CLASS
 describe Knight do
-  describe "#make_node_array_list" do
+  describe "#find_node" do
     before(:each) do
       @knight = Knight.new([3,3])
+      Node.breadth_first_list_next(@knight.root_node)
     end
 
-    it "returns"
+    it "returns a node" do
+      expect(@knight.find_node([4,3]).class).to eql(Node)
+    end
+
+    it "returns the right node" do
+      expect(@knight.find_node([4,3]).position).to eql([4,3])
+    end
   end
 end
 
 describe Node do
+  describe "#bf_next_list" do
+    before(:all) do
+      @knight = Knight.new([3,3])
+    end
+
+    it "does not raise an error" do
+      expect{ Node.breadth_first_list_next(@knight.root_position) }.to raise_error
+    end
+
+    it "creates lists of next nodes for nodes" do
+      Node.breadth_first_list_next(@knight.root_node)
+      expect(@knight).to eql(Array)
+    end
+  end
+
   describe "#list_next" do
     it "does not raise an error" do
       expect{ @knight = Knight.new([3,3]) }.not_to raise_error
+    end
+  end
+
+  describe "#list_previous" do
+    before(:all) do
+      @knight = Knight.new([3,3])
+      Node.breadth_first_list_next(@knight.root_node)
+    end
+    it "returns an array of previous positions" do
+      expect(@knight.find_node([4,3]).list_previous.class).to eql(Array)
+    end
+    it "returns the list from start to finish" do
+      expect(@knight.find_node([4,3]).list_previous).to eq([[3,3],[4,5],[2,4],[4,3]]).or eq([[3,3],[5,4],[3,5],[4,3]])
     end
   end
 end
 
 #TESTS FOR KNIGHT_MOVES METHOD
 describe "#knight_moves" do
-  it "returns an array" do
+  it "shortest path from one cell to another" do
+    expect(knight_moves([0,0],[7,7])).to eq([[0,0]])
   end
 end
